4c35a2d48261c78649af141bcccd57403908fa5c
Matteo Morganti <matteo99morganti@gmail.com>
2022-02-11 11:09:58 +0100

    aggiunti commenti e funzioni

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const vue = new Vue({
     data: {
 
         active: 0,
+        clock: null,
         // Mi dichiaro un array con le varie proprietà
         items: [{
                 url: 'img/01.jpg',
@@ -31,24 +32,43 @@ const vue = new Vue({
                 text: "Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam."
             }
         ],
+
     },
+    mounted: function(){
+            this.startAutoPlay();
+        },
     methods: {
-        getPrev() {
+        getPrev: function () {
             if (this.active <= 0) {
                 this.active = this.items.length - 1;
             } else {
                 this.active--;
             }
         },
-        getNext() {
+        getNext: function () {
             if (this.active >= this.items.length - 1) {
                 this.active = 0;
             } else {
                 this.active++;
             }
         },
-       
-    }
+        // al click su una thumb, visualizzare in grande l'immagine corrispondente
+        goToSlide(index) {
+            this.active = index;
+        },
+        // applico l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
+        startAutoPlay() {
+            var that = this;
+
+            this.clock = setInterval(function(){
+                that.getNext();
+            }, 3000);
+        },
+        // quando il mouse va in hover sullo slider, blocca l'autoplay e lo fa riprendere quando esce
+        stopAutoPlay(){
+            clearInterval(this.clock);
+            this.clock = null;
+        }
+    },
 
 });
-
